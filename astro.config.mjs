d7448937f83d608402fc968395b6cc5abe63277c
aardoiz <aardoiz14@gmail.com>
2025-08-25 11:29:51 +0200

    :wrench: Change config settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aardoiz.github.io',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
