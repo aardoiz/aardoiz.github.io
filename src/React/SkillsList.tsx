030c846596482a264fec942f1b49c5d311417e66
aardoiz <aardoiz14@gmail.com>
2026-04-14 11:28:32 +0200

    :recycle: Minor fixes

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -11,7 +11,7 @@ const CategoryIcons = {
       <path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z"></path>
     </svg>
   ),
-  "End-to-End Projects": (
+  "End-to-End Ownership": (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -44,11 +44,11 @@ const SkillsList = () => {
       "Classic ML & NLP Tools",
       "AI Evaluation"
     ],
-    "End-to-End Projects": [
+    "End-to-End Ownership": [
       "Gathering Requirements | Client Delivery",
       "Concepts -> POCs -> MVPs -> Services",
-      "Backend development",
-      "DevOps & MLOps | CI/CD"
+      "Backend & AI microservices development",
+      "Testing | AI Evaluation | CI/CD"
     ],
     "Leadership & Innovation": [
       "Lead AI Projects",
